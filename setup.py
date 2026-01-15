30f0caaf66a52e6193edb11a789c50e607bf1fd4
mmustafakapici <ug.mustafa.kapici@toros.edu.tr>
2026-01-15 18:55:41 +0300

    feat: Establish initial structure for the mkyz machine learning library, including core components, training, data, evaluation, and examples.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='mkyz',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'pandas', 'scikit-learn', 'seaborn', 'matplotlib', 
         'mlxtend', 'numpy', 'plotly', 'statsmodels', 
-        'xgboost', 'lightgbm', 'catboost', 'rich',
+        'xgboost', 'lightgbm', 'catboost', 'rich', 'optuna', 'scipy',
     ],
     description='MKYZ is a Python library for ML and data science tasks.',
     long_description=open('README.md', encoding='utf-8').read(),
